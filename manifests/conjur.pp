e3bae01ecb3e027b6be5fb5f2e7ac6b2741d512a
amishjedi <54509612+amishjedi@users.noreply.github.com>
2020-04-16 14:28:02 -0400

    Delete conjur.pp

--- manifests/conjur.pp
+++
@@ -1,8 +0,0 @@
-class { conjur:
-  account	     => 'dev',
-  appliance_url      => 'https://conjur-master-vbx:30443/api',
-  authn_login        => "host/${::trusted['hostname']}",
-  host_factory_token => Sensitive('3mbxzzc2p08hqatpd1mwq14zw2132bkc53kr11fs9sj6fq3xj7j4y'),
-  #ssl_certificate    => file('/etc/conjur.pem'),
-  version            => 5,
-}
